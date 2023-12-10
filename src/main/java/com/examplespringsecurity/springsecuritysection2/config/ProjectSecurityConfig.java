22318d91297922c4003ff42b55a11d7514fab7e5
Saif <saifali.shaikh2509@gmail.com>
2023-12-10 20:39:13 +0530

    InmemoryUserDetailsManager is used

--- src/main/java/com/examplespringsecurity/springsecuritysection2/config/ProjectSecurityConfig.java
+++ src/main/java/com/examplespringsecurity/springsecuritysection2/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,5 +43,28 @@ public class ProjectSecurityConfig {
         return httpSecurity.build();*/
 
         //For different environment, we may need to initialize different Beans, So we can use Spring Profiles to achieve this kind of requirement.
+
+        }
+
+
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
+
+            UserDetails admin = User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("12345")
+                    .authorities("admin")
+                    .build();
+
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("12345")
+                    .authorities("read")
+                    .build();
+
+            return new InMemoryUserDetailsManager(admin, user);
+
+
+
     }
 }
